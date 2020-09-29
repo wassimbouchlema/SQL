3ebc5559fcd9459290a1febce2fe7cff6e83ed88
wassimbouchlema <wassimbouchlama@gmail.com>
2020-09-29 04:31:08 +0200

    second commit

---
+++ DML.sql
@@ -0,0 +1,17 @@
+INSERT INTO Product
+VALUES ('P01','Samsung Galaxy S20',3299,'Smartphone');
+
+INSERT INTO Product
+VALUES ('P02','ASUS Notebook',4599,'PC');
+
+INSERT INTO Customer
+VALUES ('C01','ALI','71321009');
+
+INSERT INTO Customer
+VALUES ('C02','ASMA','77345823');
+
+INSERT INTO Orders
+VALUES ('C01','P02', 2, 9198, NULL);
+
+INSERT INTO Orders
+VALUES ('C02','P01', 1, 3299, '05/28/2020');
